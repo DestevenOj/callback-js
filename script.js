2e1a615636ba76854a23e1dbe21141b65f8fc8b1
Stephen Ochieng <stephenojwang1040@gmail.com>
2019-10-12 06:33:32 +0300

    callback function

---
+++ script.js
@@ -0,0 +1,17 @@
+const result = document.getElementById('result');
+
+let approval = 'Not approved!';
+
+function getApproval(callback) {
+  setTimeout(() => {
+    approval = 'Approved!';
+    callback();
+  }, 500);
+}
+
+getApproval(() => {
+  result.textContent = approval;
+});
+result.textContent = approval;
+
+
